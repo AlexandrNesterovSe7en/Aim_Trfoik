bc5e1920086876c526c47bec9f964381b8db5ffa
SE7EN <89002828+zxcPAPASHAzxc@users.noreply.github.com>
2022-09-20 17:38:27 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,98 @@
+const buttonStartGame = document.querySelector('.startGame')
+const screensArr = document.querySelectorAll('.screen')
+const buttonTimer = document.querySelector('.screen-container_btn')
+const board = document.querySelector('.board')
+const circle = document.createElement('img')
+circle.setAttribute('src', 'Новый проект.png')
+
+let timeGame = 0;
+let countVh = 0
+let score = 0
+
+let setIntervalCountTime;
+
+const {min, max} = {min: 30, max: 100}
+
+buttonStartGame.addEventListener('click', (event) =>{
+	if (countVh != -100) {
+		countVh = -100
+	}
+	screensArr.forEach((item) => item.style.transform = `translateY(${countVh}vh)`)
+}, {once: true})
+
+buttonTimer.addEventListener('click', initGame, {once: true})
+
+function initGame(event) {
+	if (countVh != -200) {
+		countVh = -200
+	}
+	if (event.target.closest('.btn')) {
+		timeGame = parseInt(event.target.closest('.btn').dataset.time)
+		screensArr.forEach((item) => item.style.transform = `translateY(${countVh}vh)`)
+		startGame()
+	}
+}
+
+function startGame() {
+	score = 0
+	drawCircle()
+	setIntervalCountTime = setInterval(timeGameLine, 1000)
+}
+function timeGameLine() {
+	if (timeGame >= 10) {
+		screensArr[2].querySelector('span').innerHTML = `00:${timeGame}`
+	} else {
+		screensArr[2].querySelector('span').innerHTML = `00:0${timeGame}`
+	}
+	
+	timeGame--
+	if (timeGame < 0) {
+		clearInterval(setIntervalCountTime)
+		finishGame()
+	}
+}
+function circleClick(event) {
+	event.preventDefault()
+	score++
+	randomize(min, max)
+}
+function drawCircle() {
+
+	randomize(min, max)
+
+	circle.addEventListener('click', circleClick)	
+}
+function randomSize(min, max) {
+	return Math.round(Math.random() * (max - min) + min)
+}
+function randomLocation(randomSizes) {
+	const {width, height} = board.getBoundingClientRect()
+	let randomX = Math.round((width - randomSizes) * Math.random())
+	let randomY = Math.round((height - randomSizes) * Math.random())
+	return [randomX, randomY]
+}
+function randomize(min, max) {
+	let randomSizes = randomSize(min, max)
+	let arrLocation = randomLocation(randomSizes)
+	let randomX = arrLocation[0]
+	let randomY = arrLocation[1]
+	circle.style.cssText = `
+	width: ${randomSizes}px; height: ${randomSizes}px;
+	top: ${randomY}px;
+	left: ${randomX}px;
+	`
+	board.append(circle)
+}
+function finishGame() {
+	board.innerHTML = `
+	<h2 class="count_score">Счёт: <span>${score}</span></h2>
+	<h3>Играть</h3>
+	`
+	circle.removeEventListener('click', circleClick)
+	board.querySelector('h3').addEventListener('click', () => {
+		countVh = -100
+		board.innerHTML = ``
+		buttonTimer.addEventListener('click', initGame, {once: true})
+		screensArr.forEach((item) => item.style.transform = `translateY(${countVh}vh)`)
+	}, {once: true})
+}
